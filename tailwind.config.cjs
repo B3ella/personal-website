620d4e912e971d49ed5436a522572b3e0185870b
Isabella Oliveira <goliveirapk@gmail.com>
2023-07-19 16:21:51 -0300

    chore: configs prettier

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,33 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-	tailwindConfig: "./styles/tailwind.config.js",
-	theme: {
-		extend: {
-			fontFamily: {
-				condensed: ["Roboto Condensed", "sans-serif"],
-				roboto: ["Roboto", "sans-serif"],
-				mono: ["Roboto Mono", "monospace"],
-			},
-			fontSize: {
-				"10xl": "9rem",
-			},
-			colors: {
-				green: "#84a98c",
-				dark: "#090c09",
-				"light-green": "#cad2c5",
-				light: "#ddd",
-			},
-			spacing: {
-				3.75: "15px",
-				"h-screen/4": "25vh",
-				"1/10": "10%",
-				12.5: "3.125rem",
-			},
-			lineHeight: {
-				title: "125px",
-			},
-		},
-	},
-	plugins: [],
-};
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    tailwindConfig: "./styles/tailwind.config.js",
+    plugins: ["prettier-plugin-tailwindcss"],
+    theme: {
+        extend: {
+            fontFamily: {
+                condensed: ["Roboto Condensed", "sans-serif"],
+                roboto: ["Roboto", "sans-serif"],
+                mono: ["Roboto Mono", "monospace"],
+            },
+            fontSize: {
+                "10xl": "9rem",
+            },
+            colors: {
+                green: "#84a98c",
+                dark: "#090c09",
+                "light-green": "#cad2c5",
+                light: "#ddd",
+            },
+            spacing: {
+                3.75: "15px",
+                "h-screen/4": "25vh",
+                "1/10": "10%",
+                12.5: "3.125rem",
+            },
+            lineHeight: {
+                title: "125px",
+            },
+        },
+    },
+    plugins: [],
+}
