d6e7d8102be64d3892c8829489f6749b4cfeb653
Isabella Oliveira <goliveirapk@gmail.com>
2023-07-19 16:33:20 -0300

    prettier

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,7 +1,6 @@
 module.exports = {
     trailingComma: "es5",
     tabWidth: 4,
-    semi: false,
     singleQuote: false,
     tailwindConfig: "./styles/tailwind.config.js",
     plugins: [require.resolve("prettier-plugin-astro")],
