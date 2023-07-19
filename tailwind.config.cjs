b5c173eb933ba0480de178cd454396c2eaabd424
Isabella Oliveira <goliveirapk@gmail.com>
2023-07-19 14:03:27 -0300

    chore: adds tailwind prettier plug in

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	tailwindConfig: "./styles/tailwind.config.js",
 	theme: {
 		extend: {
 			fontFamily: {
